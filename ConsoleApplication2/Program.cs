5669c27580c2388de49a5f499f40cba0601c23a2
Yasimonov <haliluya95@gmail.com>
2014-03-18 14:43:06 +0400

    Лаба_0
    Были произведены соответствующие исправления

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -22,14 +22,44 @@ namespace ReadingFile
                         Console.WriteLine(line);
                     }
                 }
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine("Невозможно найти часть файла или каталога:");
+                    Console.WriteLine(e.Message);
+                }
+                catch (DriveNotFoundException e)
+                {
+                    Console.WriteLine("Попытка доступа к недоступному диску или данным совместного пользования:");
+                    Console.WriteLine(e.Message);
+                }
+                catch (EndOfStreamException e)
+                {
+                    Console.WriteLine("Попытка выполнить чтение за пределами потока:");
+                    Console.WriteLine(e.Message);
+                }
+                catch (FileLoadException e)
+                {
+                    Console.WriteLine("Управляемая сборка найдена, но не может быть загружена:");
+                    Console.WriteLine(e.Message);
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("Попытка доступа к файлу, не существуещему на диске:");
+                    Console.WriteLine(e.Message);
+                }
+                catch (PathTooLongException e)
+                {
+                    Console.WriteLine("Путь или имя файла превышает максимальную длину, определенную системой:");
+                    Console.WriteLine(e.Message);
+                }
                 catch (IOException e)
                 {
-                    Console.WriteLine("Произошла ошибка:");
+                    Console.WriteLine("Произошла ошибка ввода-вывода:");
                     Console.WriteLine(e.Message);
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("ФАйл не может быть прочитан:");
+                    Console.WriteLine("Файл не может быть прочитан:");
                     Console.WriteLine(e.Message);
                 }
             }
